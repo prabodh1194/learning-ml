8431fb0d05f9648de17be68be9519e255fabc6f1
Prabodh <3585811+prabodh1194@users.noreply.github.com>
2026-01-18 23:00:15 +0530

    do full llama arch

---
+++ llama/llama.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+
+from llama.rmsnorm import RMSNorm
+from llama.block import LLaMABlock
+
+
+class LLaMA(nn.Module):
+    """
+    tokens ─► Embed ─► [Block 0] ─► [Block 1] ─► ... ─► [Block N] ─► RMSNorm ─► LM Head ─► logits
+     (B,T)    (B,T,dim)     │           │                   │         (B,T,dim)           (B,T,vocab)
+                            ▼           ▼                   ▼
+                        kv_cache[0] kv_cache[1]        kv_cache[N]
+    """
+
+    def __init__(
+        self,
+        *,
+        n_layers: int,
+        vocab_size: int,
+        dim: int,
+        max_seq_len: int,
+        num_head: int,
+        num_kv_head: int,
+    ):
+        super().__init__()
+
+        # project (B, T) -> (B, T, C)
+        self.embed = nn.Embedding(vocab_size, dim)
+
+        self.layers = nn.ModuleList(
+            [
+                LLaMABlock(dim, max_seq_len, num_head, num_kv_head)
+                for _ in range(n_layers)
+            ]
+        )
+
+        self.norm = RMSNorm(dim)
+        self.lm_head = nn.Linear(dim, vocab_size, bias=False)
+
+    def forward(
+        self,
+        tokens: torch.Tensor,
+        start_pos: int = 0,
+        kv_caches: list[tuple] | None = None,
+    ) -> tuple[torch.Tensor, list[tuple]]:
+        # tokens: (B, T)
+        x = self.embed(tokens)  # (B, T, C); C = dim
+
+        new_caches = []
+
+        for layer, kv_cache in zip(self.layers, kv_caches or [None] * len(self.layers)):
+            x, cache = layer(x, start_pos, kv_cache)
+            new_caches.append(cache)
+
+        x = self.norm(x)
+        logits = self.lm_head(x)
+
+        return logits, new_caches
+
+    def _sample(self, logits: torch.Tensor, temperature: float = 1.0) -> torch.Tensor:
+        # logits is (B, vocab_size)
+        if temperature == 0:
+            return torch.argmax(logits, dim=-1, keepdim=True)  # (B, 1)
+
+        probs = (logits / temperature).softmax(dim=-1)
+
+        result = torch.multinomial(probs, num_samples=1)  # (B, 1)
+
+        return result  # (B, 1)
+
+    @torch.inference_mode()
+    def generate(
+        self, prompt_tokens: torch.Tensor, max_new_tokens: int, temperature: float = 1.0
+    ):
+        # prompt = (B, T)
+        # logits = (B, T, vocab_size)
+        # prefill
+        start_pos = prompt_tokens.shape[-1]
+
+        logits, kv_caches = self.forward(prompt_tokens)
+
+        last_token = logits[:, -1, :]
+        next_token = self._sample(last_token, temperature)
+        tokens = [next_token.squeeze(-1)]
+
+        for i in range(1, max_new_tokens):
+            logits, kv_caches = self.forward(next_token, start_pos, kv_caches)
+            start_pos += 1
+
+            last_token = logits[:, -1, :]
+            next_token = self._sample(last_token, temperature)
+            tokens.append(next_token.squeeze(-1))
+
+        return torch.cat(tokens, dim=-1)
