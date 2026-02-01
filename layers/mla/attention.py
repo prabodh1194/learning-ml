bd7835b42b726df4e9714b551a058a4b7786926e
Prabodh <3585811+prabodh1194@users.noreply.github.com>
2026-02-01 09:41:08 +0530

    add caching to the latent attention layer

--- layers/mla/attention.py
+++ layers/mla/attention.py
@@ -13,21 +13,30 @@ class Attention(nn.Module):
         self.d_head = self.C // self.num_heads
         self.d_head_latent = self.C_latent // self.num_heads
 
+        self.W_Q = nn.Linear(self.C, self.C)
+
         self.WK_c = nn.Linear(self.C, self.C_latent, bias=False)
         self.WK_u = nn.Linear(self.C_latent, self.C, bias=False)
 
         self.WV_c = nn.Linear(self.C, self.C_latent, bias=False)
         self.WV_u = nn.Linear(self.C_latent, self.C, bias=False)
 
-    def forward(self, X: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def forward(
+        self, X: torch.Tensor, cache: tuple[torch.Tensor, torch.Tensor] | None = None
+    ) -> tuple[torch.Tensor, torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
         *_, C = X.shape
 
         assert C == self.C, f"didn't get the full dimension {self.C}. got {C} instead."
 
-        XK_latent = self.WK_c(X)
-        K = self.WK_u(XK_latent)
+        K_latent = self.WK_c(X)
+        V_latent = self.WV_c(X)
 
-        XV_latent = self.WV_c(X)
-        V = self.WV_u(XV_latent)
+        if cache is not None:
+            k_c, v_c = cache
+            K_latent = torch.cat([k_c, K_latent], dim=-2)
+            V_latent = torch.cat([v_c, V_latent], dim=-2)
 
-        return K, V
+        K = self.WK_u(K_latent)
+        V = self.WV_u(V_latent)
+
+        return K, V, (K_latent, V_latent)
