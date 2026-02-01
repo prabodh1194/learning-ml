1b5649fb38752b84eb0ccae425034528459b9f79
Prabodh <3585811+prabodh1194@users.noreply.github.com>
2026-02-01 10:18:51 +0530

    add latent head calculation

--- layers/mla/attention.py
+++ layers/mla/attention.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 from torch import nn
 
@@ -14,6 +16,7 @@ class Attention(nn.Module):
         self.d_head_latent = self.C_latent // self.num_heads
 
         self.W_Q = nn.Linear(self.C, self.C)
+        self.W_O = nn.Linear(self.C, self.C)
 
         self.WK_c = nn.Linear(self.C, self.C_latent, bias=False)
         self.WK_u = nn.Linear(self.C_latent, self.C, bias=False)
@@ -23,8 +26,8 @@ class Attention(nn.Module):
 
     def forward(
         self, X: torch.Tensor, cache: tuple[torch.Tensor, torch.Tensor] | None = None
-    ) -> tuple[torch.Tensor, torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
-        *_, C = X.shape
+    ) -> tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
+        B, T, C = X.shape
 
         assert C == self.C, f"didn't get the full dimension {self.C}. got {C} instead."
 
@@ -38,5 +41,22 @@ class Attention(nn.Module):
 
         K = self.WK_u(K_latent)
         V = self.WV_u(V_latent)
+        Q = self.W_Q(X)
 
-        return K, V, (K_latent, V_latent)
+        # split into heads
+        # (B, T, C) -> (B, num_heads, T, d_head)
+        Q_h = Q.view(B, -1, self.num_heads, self.d_head).transpose(1, 2)
+        K_h = K.view(B, -1, self.num_heads, self.d_head).transpose(1, 2)
+        V_h = V.view(B, -1, self.num_heads, self.d_head).transpose(1, 2)
+
+        # attention
+        scores = Q_h @ K_h.transpose(-2, -1) / math.sqrt(self.d_head)
+
+        if T != 1:
+            mask = torch.tril(torch.ones(T, T))
+            scores = scores.masked_fill(mask == 0, -1e9)
+
+        attn = scores.softmax(dim=-1) @ V_h
+        out = attn.transpose_(1, 2).contiguous().view(B, -1, C)
+
+        return self.W_O(out), (K_latent, V_latent)
